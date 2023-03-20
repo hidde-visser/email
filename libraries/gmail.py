dbec3ecc43c684926c95920415ac51100f152e4f
Hidde Visser <hvisser@copado.com>
2023-03-20 21:01:49 +0100

    test

--- libraries/gmail.py
+++ libraries/gmail.py
@@ -14,3 +14,14 @@ def get_email_links(email, pwd, subject, folder='INBOX'):
         links.append(link.get('href'))
 
     return links
+
+def get_email_link(email, pwd, subject, folder='INBOX', linkText):
+    # get list of email bodies from INBOX folder
+    with MailBox(host='imap.gmail.com', port=993).login(email, pwd, folder) as mailbox:
+        bodies = [msg.html for msg in mailbox.fetch(AND(subject=subject), reverse = True)]
+
+    soup = BeautifulSoup(str(bodies))
+    links = []
+    for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
+        if linkText in link:
+            return link.get('href')
